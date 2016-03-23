657269223ed7fd43df2a1c95b6c11aedc87c5949
reclaimer <silva.d.dominic@gmail.com>
2016-03-22 22:12:57 -0400

    Updated nodes in 'main_styles' -- Added and altered 'main_behavior' (added individual mouseover functions, created individual hover methods)

--- src/main/webapp/main_behavior.js
+++ src/main/webapp/main_behavior.js
@@ -1,14 +1,57 @@
 $(function() {
 
-    $('#about_node').hover(function(){
-        $('#about_text').fadeOut(500, function() {
-            $(this).text("About").fadeIn(500);
-            });
-    });
+    var aboutNode = $('#about_node');
+    var gamephotoNode = $('#game_photo_node');
+    var selfstudyNode = $('#self_study_node');
 
-    $('#about_node').click(function() {
+    $(aboutNode.mouseover(function() {
+        $(aboutNode).hover(hoverInHandler('#about_text', "About"), hoverOutHandler('#about_text', "A"));
+        alert("About Check");
+    }));
+
+    $(gamephotoNode.mouseover(function() {
+        //aboutNode.hover(hoverInHandler('about_text', "About"), hoverOutHandler('about_text', "A"))
+        alert("gp Check");
+    }));
+
+    $(selfstudyNode.mouseover(function() {
+        //aboutNode.hover(hoverInHandler('about_text', "About"), hoverOutHandler('about_text', "A"))
+        alert("ss Check");
+    }));
+        //});
+
+    function hoverInHandler(nodeName, nodeText){
+        alert("hover in")
+        $(nodeName).fadeOut(300, function() {
+            $(this).text(nodeName).fadeIn(300);
+            });
+    }
+
+    function hoverOutHandler(nodeName, nodeText) {
+            $(nodeName).fadeOut(300, function() {
+            $(this).text(nodeText).fadeIn(300);
+           });
+    }
+
+    /*$('#about_node').click(function() {
         alert("clicked");
-    });
+    });*/
 });
 
-//TODO: Figure out how to make text transition; add conditionals to click function
+//TODO: Fix hoverIn/hoverOut functions, Add conditionals to click function
+
+/*    function nodeHandler(node)
+      $('#about_node').hover(function(){
+          $('#about_text').fadeOut(300, function() {
+              $(this).text("About").fadeIn(300);
+              })
+      }, function() {
+              $('#about_text').fadeOut(300, function() {
+              $(this).text("A").fadeIn(300);
+             });
+      });
+
+      $('#about_node').click(function() {
+          alert("clicked");
+      });
+*/
