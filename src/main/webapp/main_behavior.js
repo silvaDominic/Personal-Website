09091cc0484d9fc51817244485ace42b1f23d809
reclaimer <silva.d.dominic@gmail.com>
2016-03-28 22:39:33 -0400

    Dropped node-menu design in place of a central box-menu design -- Deleted SVG node files -- Added temp html and css -- Added JavaScript to animate menu (still testing)

--- src/main/webapp/main_behavior.js
+++ src/main/webapp/main_behavior.js
@@ -1,57 +1,19 @@
 $(function() {
-
-    var aboutNode = $('#about_node');
-    var gamephotoNode = $('#game_photo_node');
-    var selfstudyNode = $('#self_study_node');
-
-    $(aboutNode.mouseover(function() {
-        hoverInHandler('#about_text', "About");
-        hoverOutHandler('#about_text', "A");
-        //alert("About Check");
-    }));
-
-    $(gamephotoNode.mouseover(function() {
-        //aboutNode.hover(hoverInHandler('about_text', "About"), hoverOutHandler('about_text', "A"))
-        //alert("gp Check");
-    }));
-
-    $(selfstudyNode.mouseover(function() {
-        //aboutNode.hover(hoverInHandler('about_text', "About"), hoverOutHandler('about_text', "A"))
-        //alert("ss Check");
-    }));
-        //});
-
-    function hoverInHandler(nodeName, nodeText){
-        $(nodeName).fadeOut(300, function() {
-            $(this).text(nodeText).fadeIn(300);
+    $("#about-section").hide();
+    $("#about-button").toggle(function() {
+        $("#about-button")
+            .animate({width: '800px'}, 500)
+            .animate({height: '300px'}, {duration: 500,
+                complete: function() {
+                $("#about-section").fadeIn(1000);
+                }
+             });
+        }, function() {
+            $("#about-section").fadeOut(500);
+            $("#about-button")
+                .animate({height: '100px'}, 500)
+                .animate({width: '100px'}, 500);
             });
-    }
-
-    function hoverOutHandler(nodeName, nodeText) {
-         $(nodeName).fadeOut(300, function() {
-            $(this).text(nodeText).fadeIn(5000);
-           });
-    }
-
-    /*$('#about_node').click(function() {
-        alert("clicked");
-    });*/
 });
 
-//TODO: Fix hoverIn/hoverOut functions, Add conditionals to click function
-
-/*    function nodeHandler(node)
-      $('#about_node').hover(function(){
-          $('#about_text').fadeOut(300, function() {
-              $(this).text("About").fadeIn(300);
-              })
-      }, function() {
-              $('#about_text').fadeOut(300, function() {
-              $(this).text("A").fadeIn(300);
-             });
-      });
-
-      $('#about_node').click(function() {
-          alert("clicked");
-      });
-*/
+//TODO: Drop .toggle method, use 'toggle' attributes instead
