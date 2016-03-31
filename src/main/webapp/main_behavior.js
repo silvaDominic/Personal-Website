d26b0539817384b5d67ea41810fdd766c63876ca
reclaimer <silva.d.dominic@gmail.com>
2016-03-31 03:19:32 -0400

    Tried to create single functin for animating info-blocks

--- src/main/webapp/main_behavior.js
+++ src/main/webapp/main_behavior.js
@@ -1,37 +1,72 @@
-$(function() {
-    $("#aboutme-content").hide();
-    $("#aboutme-container").click(function() {
-        if ($(this).hasClass("active")) {
-            $("#button-text").fadeOut(150);
-            $("#aboutme-container")
-                .animate({width: '800px'}, 500)
-                .animate({top: '50px', height: '300px'}, {duration: 500,
-                    complete: function() {
-                    $("#aboutme-content").fadeIn(1000);
-                    $(this).removeClass("active");
-                    }
-                 });
-        }
-        else {
-            $("#aboutme-content").fadeOut(500).hide();
-            $("#aboutme-container")
-                .animate({top: '25%', height: '100px'}, 500)
-                .animate({width: '100px'}, {duration: 500,
-                complete: function() {
-                $("#button-text").fadeIn(500);
-                $(this).addClass("active");
-                        }
-                });
-        }
-    });
+var aboutMeSection = {container: "#aboutme-container",
+                       content: "#aboutme-content",
+                       image: "#aboutme-image"};
 
-    $("#tools-content").hide();
+var toolsSection = {container: "#tools-container",
+                       content: "#tools-content",
+                       image: "#tools-image"};
+
+var gamesSection = {container: "#games-container",
+                       content: "#games-content",
+                       image: "#games-image"};
+
+var photoSection = {container: "#photo-container",
+                       content: "#photo-content",
+                       image: "#photo-image"};
+
+function getContainer() { return container; }
+
+function getContent() { return content; }
+
+function getImage() { return image; }
+
+alert(aboutMeSection.getContainer());
+$(function() {
+
+    switch($(this)){
+        case "#aboutme-container":
+            alert(this);
+            animateSection(aboutMeSection.getContainer(), aboutMeSection.getContent(), aboutMeSection.getImage());
+            break;
+        default:
+            break;
+    }
+
+    function animateSection (container, content, image){
+        $(content).hide();
+        $(container).click(function() {
+            if ($(container).hasClass("active")) {
+                $(image).fadeOut(150);
+                $(container)
+                    .animate({width: '80%'}, 500)
+                    .animate({top: '60px', height: '300px'}, {duration: 500,
+                        complete: function() {
+                        $(content).fadeIn(1000);
+                        $(this).removeClass("active");
+                        }
+                     });
+            }
+            else {
+                $(content).fadeOut(500).hide();
+                $(container)
+                    .animate({top: '25%', height: '120px'}, 500)
+                    .animate({width: '120px'}, {duration: 500,
+                    complete: function() {
+                    $(image).fadeIn(500);
+                    $(this).addClass("active");
+                            }
+                    });
+            }
+        });
+    }
+
+/*    $("#tools-content").hide();
     $("#tools-container").click(function() {
         if ($(this).hasClass("active")) {
             $("#tools-image").fadeOut(150);
             $("#tools-container")
-                .animate({width: '800px'}, 500)
-                .animate({height: '300px'}, {duration: 500,
+                .animate({width: '80%'}, 500)
+                .animate({height: '325px'}, {duration: 500,
                     complete: function() {
                     $("#tools-content").fadeIn(1000);
                     $(this).removeClass("active");
@@ -41,13 +76,13 @@ $(function() {
         else {
             $("#tools-content").fadeOut(500).hide();
             $("#tools-container")
-                .animate({height: '100px'}, 500)
-                .animate({width: '100px'}, {duration: 500,
+                .animate({height: '120px'}, 500)
+                .animate({width: '120px'}, {duration: 500,
                 complete: function() {
                 $("#tools-text").fadeIn(500);
                 $(this).addClass("active");
                         }
                 });
         }
-    });
+    });*/
 });
