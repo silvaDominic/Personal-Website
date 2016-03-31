7fd4813ab2cb87ff75464eb1dc7e09b51c582c40
reclaimer <silva.d.dominic@gmail.com>
2016-03-31 02:24:09 -0400

    Fixed animation problem with aboutme info-block (.toggle was depreciated) -- Added tool info-block and animated it to grow upwards

--- src/main/webapp/main_behavior.js
+++ src/main/webapp/main_behavior.js
@@ -1,15 +1,53 @@
 $(function() {
-    $("#about-button").toggle(function() {
-        console.log("1st");
-        $(this)
-            .animate({width: '800px'}, 500)
-            .animate({height: '300px'});
-        }, function() {
-            console.log("2nd");
-            $(this)
-                .animate({height: '100px'}, 500)
-                .animate({width: '100px'}, 500);
-            });
-});
+    $("#aboutme-content").hide();
+    $("#aboutme-container").click(function() {
+        if ($(this).hasClass("active")) {
+            $("#button-text").fadeOut(150);
+            $("#aboutme-container")
+                .animate({width: '800px'}, 500)
+                .animate({top: '50px', height: '300px'}, {duration: 500,
+                    complete: function() {
+                    $("#aboutme-content").fadeIn(1000);
+                    $(this).removeClass("active");
+                    }
+                 });
+        }
+        else {
+            $("#aboutme-content").fadeOut(500).hide();
+            $("#aboutme-container")
+                .animate({top: '25%', height: '100px'}, 500)
+                .animate({width: '100px'}, {duration: 500,
+                complete: function() {
+                $("#button-text").fadeIn(500);
+                $(this).addClass("active");
+                        }
+                });
+        }
+    });
 
-//TODO: Drop .toggle method, use 'toggle' attributes instead
+    $("#tools-content").hide();
+    $("#tools-container").click(function() {
+        if ($(this).hasClass("active")) {
+            $("#tools-image").fadeOut(150);
+            $("#tools-container")
+                .animate({width: '800px'}, 500)
+                .animate({height: '300px'}, {duration: 500,
+                    complete: function() {
+                    $("#tools-content").fadeIn(1000);
+                    $(this).removeClass("active");
+                    }
+                 });
+        }
+        else {
+            $("#tools-content").fadeOut(500).hide();
+            $("#tools-container")
+                .animate({height: '100px'}, 500)
+                .animate({width: '100px'}, {duration: 500,
+                complete: function() {
+                $("#tools-text").fadeIn(500);
+                $(this).addClass("active");
+                        }
+                });
+        }
+    });
+});
