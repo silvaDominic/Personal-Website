334d83b3f4cdaced7630cc571595ce8403c7bacd
reclaimer <silva.d.dominic@gmail.com>
2016-04-03 18:00:40 -0400

    Added object literals for remaining sections -- Removed 'fade-icon' animation  from grow and shrink animation functions-- Added paragraph classes to all paragraphs -- Basic styling for games/photography section & icons

--- src/main/webapp/main-behavior.js
+++ src/main/webapp/main-behavior.js
@@ -26,14 +26,36 @@ $(function() {
         });*/
 
     var aboutmeSection = {id: '#aboutme-section', get SectionID() {return this.id;},
+                          originalHeight: $('#aboutme-section').height(),
+                          originalWidth: $('#aboutme-section').width(),
+                          originalTop: $('#aboutme-section').position().top,
                           expandedHeight: '325px', get ExpandedHeight() {return this.expandedHeight;},
                           expandedWidth: '80%', get ExpandedWidth() {return this.expandedWidth;},
                           expandedTop: '60px', get ExpandedTop() {return this.expandedTop;}};
 
     var toolsSection = {id: '#tools-section', get SectionID() {return this.id;},
+                        originalHeight: $('#tools-section').height(),
+                        originalWidth: $('#tools-section').width(),
+                        originalTop: $('#tools-section').position().top,
                         expandedHeight: '325px', get ExpandedHeight() {return this.expandedHeight;},
                         expandedWidth: '80%', get ExpandedWidth() {return this.expandedWidth;},
                         expandedTop: '50%', get ExpandedTop() {return this. expandedTop;}};
+
+    var gamesSection = {id: '#games-section', get SectionID() {return this.id;},
+                          originalHeight: $('#games-section').height(),
+                          originalWidth: $('#games-section').width(),
+                          originalTop: $('#games-section').position().top,
+                          expandedHeight: '80%', get ExpandedHeight() {return this.expandedHeight;},
+                          expandedWidth: '325px', get ExpandedWidth() {return this.expandedWidth;},
+                          expandedTop: '', get ExpandedTop() {return this.expandedTop;}};
+
+    var photographySection = {id: '#photography-section', get SectionID() {return this.id;},
+                        originalHeight: $('#photography-section').height(),
+                        originalWidth: $('#photography-section').width(),
+                        originalTop: $('#photography-section').position().top,
+                        expandedHeight: '80%', get ExpandedHeight() {return this.expandedHeight;},
+                        expandedWidth: '325px', get ExpandedWidth() {return this.expandedWidth;},
+                        expandedTop: '', get ExpandedTop() {return this. expandedTop;}};
 
     $(".content").hide();
     $(".container").click(function() {
@@ -68,10 +90,9 @@ $(function() {
 
     function growSection(section) {
             var elem = section.SectionID;
-            $(elem).find(".icon").fadeOut(150);
             $(elem)
-                .animate({width: section.ExpandedWidth}, 500)
-                .animate({top: section.ExpandedTop, height: section.ExpandedHeight}, {duration: 500,
+                .animate({top: section.ExpandedTop, height: section.ExpandedHeight}, 500)
+                .animate({width: section.ExpandedWidth}, {duration: 500,
                     complete: function() {
                     $(elem).find(".content").fadeIn(750);
                     $(elem).removeClass("active");
@@ -83,12 +104,13 @@ $(function() {
         var elem = section.SectionID;
         $(elem).find(".content").fadeOut(500);
         $(elem)
-            .animate({top: $(elem).css('top'), height: $(elem).css('height')}, 500)
-            .animate({width: $(elem).css('width')}, {duration: 500,
+            .animate({width: section.originalWidth}, 500)
+            .animate({top: section.originalTop, height: section.originalHeight}, {duration: 500,
             complete: function() {
-            $(elem).find(".icon").fadeIn(500);
             $(elem).addClass("active");
-                    }
+            }
             });
     }
 });
+
+//TODO: Figure out how to adjust animate functions according to whether they are top-bottom oriented or left-right oriented
