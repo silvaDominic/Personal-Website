f081998512b016ae6db4cda1cede1206c7af0c0f
reclaimer <silva.d.dominic@gmail.com>
2016-04-03 23:36:05 -0400

    Handled orientation problem in animate functions -- Tried to correct animations

--- src/main/webapp/main-behavior.js
+++ src/main/webapp/main-behavior.js
@@ -1,35 +1,10 @@
 $(function() {
-/*        $(this.content).hide();
-        $(this.container).click(function() {
-            if ($(this.container).hasClass("active")) {
-                $(this.icon).fadeOut(150);
-                $(this.container)
-                    .animate({width: '80%'}, 500)
-                    .animate({top: '60px', height: '300px'}, {duration: 500,
-                        complete: function() {
-                        $(this.content).fadeIn(1000);
-                        $(this).removeClass("active");
-                        }
-                     });
-            }
-            else {
-                $(this.content).fadeOut(500).hide();
-                $(this.container)
-                    .animate({top: '25%', height: '120px'}, 500)
-                    .animate({width: '120px'}, {duration: 500,
-                    complete: function() {
-                    $(this.icon).fadeIn(500);
-                    $(this).addClass("active");
-                            }
-                    });
-            }
-        });*/
-
     var aboutmeSection = {id: '#aboutme-section', get SectionID() {return this.id;},
                           originalHeight: $('#aboutme-section').height(),
                           originalWidth: $('#aboutme-section').width(),
                           originalTop: $('#aboutme-section').position().top,
-                          expandedHeight: '325px', get ExpandedHeight() {return this.expandedHeight;},
+                          animatesHorizontally: false, get AnimatesHorizontally() {return this.animatesHorizontally},
+                          expandedHeight: '320px', get ExpandedHeight() {return this.expandedHeight;},
                           expandedWidth: '80%', get ExpandedWidth() {return this.expandedWidth;},
                           expandedTop: '60px', get ExpandedTop() {return this.expandedTop;}};
 
@@ -37,7 +12,8 @@ $(function() {
                         originalHeight: $('#tools-section').height(),
                         originalWidth: $('#tools-section').width(),
                         originalTop: $('#tools-section').position().top,
-                        expandedHeight: '325px', get ExpandedHeight() {return this.expandedHeight;},
+                        animatesHorizontally: false, get AnimatesHorizontally() {return this.animatesHorizontally},
+                        expandedHeight: '320px', get ExpandedHeight() {return this.expandedHeight;},
                         expandedWidth: '80%', get ExpandedWidth() {return this.expandedWidth;},
                         expandedTop: '50%', get ExpandedTop() {return this. expandedTop;}};
 
@@ -45,17 +21,19 @@ $(function() {
                           originalHeight: $('#games-section').height(),
                           originalWidth: $('#games-section').width(),
                           originalTop: $('#games-section').position().top,
+                          animatesHorizontally: true, get AnimatesHorizontally() {return this.animatesHorizontally},
                           expandedHeight: '80%', get ExpandedHeight() {return this.expandedHeight;},
-                          expandedWidth: '325px', get ExpandedWidth() {return this.expandedWidth;},
-                          expandedTop: '', get ExpandedTop() {return this.expandedTop;}};
+                          translate: '50%', get Translate() {return this.translate;},
+                          expandedTop: '50%', get ExpandedTop() {return this.expandedTop;}};
 
     var photographySection = {id: '#photography-section', get SectionID() {return this.id;},
                         originalHeight: $('#photography-section').height(),
                         originalWidth: $('#photography-section').width(),
                         originalTop: $('#photography-section').position().top,
+                        animatesHorizontally: true, get AnimatesHorizontally() {return this.animatesHorizontally},
                         expandedHeight: '80%', get ExpandedHeight() {return this.expandedHeight;},
-                        expandedWidth: '325px', get ExpandedWidth() {return this.expandedWidth;},
-                        expandedTop: '', get ExpandedTop() {return this. expandedTop;}};
+                        expandedWidth: '400px', get ExpandedWidth() {return this.expandedWidth;},
+                        expandedTop: '50%', get ExpandedTop() {return this. expandedTop;}};
 
     $(".content").hide();
     $(".container").click(function() {
@@ -67,6 +45,14 @@ $(function() {
 
                 case 'tools-section':
                 growSection(toolsSection);
+                break;
+
+                case 'games-section':
+                growSection(gamesSection);
+                break;
+
+                case 'photography-section':
+                growSection(photographySection);
                 break;
 
                 default:
@@ -82,6 +68,14 @@ $(function() {
                 shrinkSection(toolsSection);
                 break;
 
+                case 'games-section':
+                shrinkSection(gamesSection);
+                break;
+
+                case 'photography-section':
+                shrinkSection(photographySection);
+                break;
+
                 default:
                 break;
             }
@@ -89,7 +83,19 @@ $(function() {
     });
 
     function growSection(section) {
-            var elem = section.SectionID;
+        var elem = section.SectionID;
+
+        if (section.animatesHorizontally){
+            $(elem)
+                .animate({left: section.Translate}, 500)
+                .animate({top: section.ExpandedTop, height: section.ExpandedHeight}, {duration: 500,
+                    complete: function() {
+                    $(elem).find(".content").fadeIn(750);
+                    $(elem).removeClass("active");
+                    }
+                 });
+
+        } else {
             $(elem)
                 .animate({top: section.ExpandedTop, height: section.ExpandedHeight}, 500)
                 .animate({width: section.ExpandedWidth}, {duration: 500,
@@ -99,18 +105,25 @@ $(function() {
                     }
                  });
         }
+    }
 
     function shrinkSection(section){
         var elem = section.SectionID;
-        $(elem).find(".content").fadeOut(500);
-        $(elem)
-            .animate({width: section.originalWidth}, 500)
-            .animate({top: section.originalTop, height: section.originalHeight}, {duration: 500,
-            complete: function() {
-            $(elem).addClass("active");
-            }
-            });
+
+        if (section.animatesHorizontally){
+            $(elem).find(".content").fadeOut(500);
+            $(elem)
+                .animate({top: section.originalTop, height: section.originalHeight}, 500)
+                .animate({width: section.originalWidth}, 500);
+                $(elem).addClass("active");
+        } else {
+           $(elem).find(".content").fadeOut(500);
+           $(elem)
+               .animate({width: section.originalWidth}, 500)
+               .animate({top: section.originalTop, height: section.originalHeight}, 500);
+               $(elem).addClass("active");
+        }
     }
 });
 
-//TODO: Figure out how to adjust animate functions according to whether they are top-bottom oriented or left-right oriented
+//TODO: Fix animations.
